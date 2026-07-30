dccc68227bf201464b5b301e66049e9f2c89dd39
baris-ux <baris.ock.etudes@gmail.com>
2026-07-30 16:02:35 +0200

    chore: installer eslint et prettier et configurer eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// eslint.config.js
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+
+export default defineConfig([
+
+    js.configs.recommended,  /* c'est un ensemble de règle préconfiguré pour JavaScript ca permet d'éviter d'écrire 100 règle à la main
+    ca permet par exemple d'empêcher de réassigner une valeur à une variable déclarée avec const*/
+
+    ...svelte.configs['flat/recommended'], // meme principe mais avec du svelte cette fois ci
+
+    {
+        files: ['**/*.svelte'],
+        languageOptions: {
+            parserOptions: {
+                parser: ts.parser
+            }
+        }
+    },
+    /* ici pour les fichier en .svelte on va venir parser le code typescript ce qui permettra à 
+    notre linter de lire et comprendre le code typescript pour le linter*/
+    
+	{
+		rules: { // pour le moemnt on va partir avec les règle par défaut spécifié par js.configs.recommended et svelte.configs['flat/recommended'] 
+                // mais on peut venir ajouter nos propres règles ici
+		},
+	},
+    {
+        ignores: ['node_modules/**', 'dist/**', '.svelte-kit/**', 'src-tauri/**', 'build/**'], 
+    }
+]);
